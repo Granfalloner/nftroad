4f4c2a52211d067e96c8887ca83119a5da736132
Vasyl Myronchuk <granfalloner@gmail.com>
2022-01-14 16:19:25 +0200

    Title and description updated.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p className="App-paragraph">
-            <b>NFTs that will be your subscription to some creative content platform></b><br/><br/>
+            <b>NFTs that will be your subscription to some creative content platform</b><br/><br/>
             ✅ you can resell your NFT , and your subscription.<br/>
             ✅ platform and creatives will get royalties from reselling subscription.<br/>
         </p>
