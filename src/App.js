542ffe47abb68c696fe5dcf81104305a90ca92dc
yrik <iurii.kriachko@gmail.com>
2022-01-16 00:09:22 +0200

    Can see and create courses via interaction with a smart contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import { ethers } from "ethers";
+import nftroadabi from "./nftroad.json"
 import './App.css';
 
-function App() {
+const provider = new ethers.providers.InfuraProvider(137, "3161b9b2a1d04bc4b1720629f18afd7d")
+
+const address = "0x2c9286f482e635009ed6eaaf478b84869493b0fb"
+
+let contract = new ethers.Contract(address, nftroadabi, provider);
+
+
+function Header() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="App-paragraph">
+      <header className="m-5 text-center">
+        <p>
             <b>NFTs that will be your subscription to some creative content platform</b><br/><br/>
             ✅ you can resell your NFT , and your subscription.<br/>
             ✅ platform and creatives will get royalties from reselling subscription.<br/>
         </p>
         <a
-          className="App-link"
           href="https://discord.gg/39AMveCHKD"
           target="_blank"
           rel="noopener noreferrer"
+          className="underline"
         >
           Discord
         </a>
       </header>
+  )
+}
+
+function Home() {
+
+  const [records, setRecords] = useState()
+
+  useEffect(() => {
+    async function init() {
+      const result = await contract.viewRecords(20, 0)
+      setRecords(result.filter(x=>x.maxSupply > 0))
+    }
+    init()
+  })
+  
+  async function createRecord() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    // Prompt user for account connections
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    contract = contract.connect(signer)
+    await contract.createRecord(ethers.utils.parseEther('0.01'), 100);
+  }
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <button onClick={()=>createRecord(0.01, 100)}>Add Course(0.01 matic, 100 items)</button>
+        <br/>
+        Records: {JSON.stringify(records)}
+      </div>
+
     </div>
   );
 }
+
+function Create() {
+  return (
+    <div>
+      <h4 className="text-lg text-center m-5">Sell Course</h4>
+
+      <div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </div>
+  );
+}
+
 
 export default App;
