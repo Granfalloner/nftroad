729254fbbc0dd79fe529b59e11a1ff325171f1f8
Vasyl Myronchuk <granfalloner@gmail.com>
2022-01-16 16:21:54 +0200

    Added icons to sidemenu.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function Footer() {
             ✅ you can resell your NFT , and your subscription.<br/>
             ✅ platform and creatives will get royalties from reselling subscription.<br/>
         </p>
+        <br />
         <a
           href="https://discord.gg/39AMveCHKD"
           target="_blank"
@@ -61,19 +62,13 @@ function Home() {
                           </li>
                           <li>
                               <a>
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 mr-2 stroke-current">
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                                  </svg>
+                                  <img className="pr-5" src="/resources/icons/AllCourses.svg" />
                                   All courses
                               </a>
                           </li>
                           <li>
                               <a>
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 mr-2 stroke-current">
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                                  </svg>
+                                  <img className="pr-5" src="/resources/icons/MyNFTs.svg" />
                                   My NFTs
                               </a>
                           </li>
@@ -85,19 +80,13 @@ function Home() {
                           </li>
                           <li>
                               <a>
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 mr-2 stroke-current">
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                                  </svg>
+                                  <img className="pr-5" src="/resources/icons/MyCourses.svg" />
                                   My courses
                               </a>
                           </li>
                           <li>
                               <a href="/create">
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 mr-2 stroke-current">
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                                  </svg>
+                                  <img className="pr-5" src="/resources/icons/CreateCourse.svg" />
                                   Create course
                               </a>
                           </li>
