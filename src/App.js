ad1bb835b27f305e3934e3b87f53699936199f57
Myronchuk Vasyl <granfalloner@gmail.com>
2022-01-19 16:47:28 +0200

    Main side menu split into subcomponents (CustomerMenu, CreatorMenu and MenuItem).

--- src/App.js
+++ src/App.js
@@ -13,27 +13,6 @@ const address = "0x8B204E9349885bCd7cB1ef0208Ee736a78fc082D"
 let contract = new ethers.Contract(address, nftroadabi, provider);
 
 
-function Footer() {
-    return (
-        <footer className="m-5 text-center">
-            <p>
-                <b>NFTs that will be your subscription to some creative content platform</b><br /><br />
-                ✅ you can resell your NFT , and your subscription.<br />
-                ✅ platform and creatives will get royalties from reselling subscription.<br />
-            </p>
-            <br />
-            <a
-                href="https://discord.gg/39AMveCHKD"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-            >
-                Discord
-            </a>
-        </footer>
-    )
-}
-
 function WalletInfo(props) {
     return (
         <div className="p-4 pt-10">
@@ -48,6 +27,45 @@ function WalletInfo(props) {
             <button className="btn btn-warning w-full bg-amber-500" onClick={props.connectWallet}>{props.walletConnected ? "Change wallet" : "Connect your wallet"}</button>
             <br />
         </div>
+    )
+}
+
+function MenuItem(props) {
+    return (
+        <li>
+            <img alt="" className="pl-5 pr-5" src={props.image} />
+            <button className="text-white" onClick={() => props.setFilter(props.value)}>
+                {props.title}
+            </button>
+        </li>
+    )
+}
+
+function CustomerMenu(props) {
+    return (
+        <ul className="side-menu menu py-3">
+            <li className="menu-title text-sm p-4 text-amber-500">
+                #CUSTOMER
+            </li>
+            <MenuItem title="All courses" image="/resources/icons/AllCourses.svg" setFilter={() => props.setFilter(0)} />
+            <br />
+            <MenuItem title="My NFTs" image="/resources/icons/MyNFTs.svg" setFilter={() => props.setFilter(1)} />
+        </ul>
+    )
+}
+
+function CreatorMenu(props) {
+    return (
+        <ul className="side-menu menu py-3">
+            <li className="menu-title p-4 text-amber-500">
+                #CREATOR
+            </li>
+            <MenuItem title="My courses" image="/resources/icons/MyCourses.svg" setFilter={() => props.setFilter(2)} />
+            <br />
+            <Link to="/create">
+                <MenuItem title="Create course" image="/resources/icons/CreateCourse.svg" setFilter={() => {}} />
+            </Link>
+        </ul>
     )
 }
 
@@ -67,6 +85,27 @@ function CourseCard(props) {
                 </div>
             </div>
         </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className="m-5 text-center">
+            <p>
+                <b>NFTs that will be your subscription to some creative content platform</b><br /><br />
+                ✅ you can resell your NFT , and your subscription.<br />
+                ✅ platform and creatives will get royalties from reselling subscription.<br />
+            </p>
+            <br />
+            <a
+                href="https://discord.gg/39AMveCHKD"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+            >
+                Discord
+            </a>
+        </footer>
     )
 }
 
@@ -104,55 +143,16 @@ function Home() {
         <div className="background-main">
             <div className="grid grid-cols-5 gap-6">
                 <div className="background-secondary">
-                    <div>
-                        <WalletInfo walletConnected={walletConnected} connectWallet={connectWallet} />
-                        <ul className="menu py-3">
-                            <li className="menu-title text-sm p-4 text-amber-500">
-                                #CUSTOMER
-                            </li>
-                            <li style={{display: 'block'}}>
-                                <img alt="" style={{display: 'inline'}} className="pl-5 pr-5" src="/resources/icons/AllCourses.svg" />
-                                <button className="text-white" onClick={() => setFilter(0)}>
-                                    All courses
-                                </button>
-                            </li>
-                            <br /> 
-                            <li style={{display: 'block'}}>
-                                <img alt="" style={{display: 'inline'}} className="pl-5 pr-5" src="/resources/icons/MyNFTs.svg" />
-                                <button className="text-white" onClick={() => setFilter(1)}>
-                                    My NFTs
-                                </button>
-                            </li>
-                        </ul>
-
-                        <ul className="menu py-3">
-                            <li className="menu-title p-4 text-amber-500">
-                                #CREATOR
-                            </li>
-                            <li style={{display: 'block'}}>
-                                <img alt="" style={{display: 'inline'}} className="pl-5 pr-5" src="/resources/icons/MyCourses.svg" />
-                                <button className="text-white" onClick={() => setFilter(2)}>
-                                    My courses
-                                </button>
-                            </li>
-                            <br />
-                            <li style={{display: 'block'}}>
-                                <img alt="" style={{display: 'inline'}} className="pl-5" src="/resources/icons/CreateCourse.svg" />
-                                <Link to="/create" style={{display: 'inline'}} className="pt-0">
-                                    <button className="text-white">
-                                        Create course
-                                    </button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                    <WalletInfo walletConnected={walletConnected} connectWallet={connectWallet} />
+                    <CustomerMenu setFilter={setFilter} />
+                    <CreatorMenu setFilter={setFilter} />
                 </div>
 
                 <div className="grid grid-cols-3 col-span-4 gap-6 pt-10">
                     {
                         records
+                        .filter(record => record.active)
                         .filter(record => {
-                            if (!record.active) { return false; }
                             switch (filter) {
                                 case 0: return true;
                                 case 1: return false;
